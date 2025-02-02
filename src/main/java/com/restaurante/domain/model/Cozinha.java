407cf620ed9a8d55eadff1c69fee32c74fa582fa
Thiago Ripardo de Lima <thiago.ripardo.86@gmail.com>
2025-02-02 16:04:49 -0400

    feat:Propriedade nullable de @Column e @JoinColumn

--- src/main/java/com/restaurante/domain/model/Cozinha.java
+++ src/main/java/com/restaurante/domain/model/Cozinha.java
@@ -3,10 +3,6 @@ package com.restaurante.domain.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.Objects;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -18,18 +14,6 @@ public class Cozinha {
     @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return Objects.equals(id, cozinha.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
